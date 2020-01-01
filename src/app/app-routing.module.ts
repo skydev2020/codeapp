283662f44ef20ac9d570500117e5b3ad885b10e8
Sky Dev <skydev2020@outlook.com>
2019-12-31 23:00:42 -0500

    add 2 components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import {HomeComponent} from './home/home.component';
+import {ListComponent} from './list/list.component';
 
 const routes: Routes = [];
 
